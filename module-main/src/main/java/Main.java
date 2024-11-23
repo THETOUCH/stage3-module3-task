fa4321f2b3e3cbe1e9c2c1989df99fc9a5344c3a
THETOUCH <super.ilgiznex@gmail.com>
2024-11-24 00:39:50 +0600

    123

--- module-main/src/main/java/Main.java
+++ module-main/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import com.mjc.school.controller.implementation.AuthorController;
 import com.mjc.school.controller.implementation.NewsController;
+import com.mjc.school.controller.implementation.TagController;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
@@ -8,6 +9,7 @@ public class Main {
 
         NewsController newsController = context.getBean(NewsController.class);
         AuthorController authorController = context.getBean(AuthorController.class);
+        TagController tagController = context.getBean(TagController.class);
 
         System.out.println(newsController);
         System.out.println(authorController);
